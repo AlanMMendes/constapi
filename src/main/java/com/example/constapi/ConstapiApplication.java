f71aefa33a64543efb0dbd504586dba834c5989e
Alan M. Mendes <alanmazeto.m@gmail.com>
2024-09-23 14:39:51 -0300

    commit: added new routes for Messages

---
+++ src/main/java/com/example/constapi/ConstapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.constapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstapiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/constapi/controller/MessageControler.java
@@ -0,0 +1,18 @@
+package com.example.constapi.controller;
+
+import com.example.constapi.domain.Message;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageControler {
+    @GetMapping("/hello")
+    public Message startMessage(@RequestParam(value="name", defaultValue="World") String name){
+        return new Message(String.format("Hello %s!", name));
+    }
+    @GetMapping("/goodbye")
+    public Message endMessage(@RequestParam(value="name", defaultValue="World") String name){
+        return new Message(String.format("See you next time %s!!", name));
+    }
+}
